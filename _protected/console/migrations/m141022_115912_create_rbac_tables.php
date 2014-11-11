097346a2dea890532941a7bdb293e43862dbf2c3
Nenad Zivkovic <nenad@freetuts.org>
2014-11-12 00:19:02 +0100

    Fixes issue #2

--- _protected/console/migrations/m141022_115912_create_rbac_tables.php
+++ _protected/console/migrations/m141022_115912_create_rbac_tables.php
@@ -81,7 +81,7 @@ class m141022_115912_create_rbac_tables extends \yii\db\Migration
 
         $this->createTable($authManager->assignmentTable, [
             'item_name' => Schema::TYPE_STRING . '(64) NOT NULL',
-            'user_id' => Schema::TYPE_STRING . '(64) NOT NULL',
+            'user_id' => Schema::TYPE_INTEGER . ' NOT NULL',
             'created_at' => Schema::TYPE_INTEGER,
             'PRIMARY KEY (item_name, user_id)',
             'FOREIGN KEY (item_name) REFERENCES ' . $authManager->itemTable . ' (name) 
