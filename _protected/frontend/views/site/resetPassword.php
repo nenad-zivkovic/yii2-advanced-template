83b3ed6f2d5e024582f7a67e57623c07f0667f7d
Nenad Zivkovic <nenad@freetuts.org>
2014-11-27 15:52:09 +0100

    v2

--- _protected/frontend/views/site/resetPassword.php
+++ _protected/frontend/views/site/resetPassword.php
@@ -1,4 +1,5 @@
 <?php
+use nenad\passwordStrength\PasswordInput;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
@@ -19,7 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row">
         <div class="col-lg-12">
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password')->widget(PasswordInput::classname(), []) ?>
                 <div class="form-group">
                     <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
                 </div>
