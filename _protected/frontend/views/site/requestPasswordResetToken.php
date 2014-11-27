3b9737c6c5d4cf1f57699c9ce041bcce09d6a564
Nenad Zivkovic <nenad@freetuts.org>
2014-11-27 15:33:38 +0100

    v2

--- _protected/frontend/views/site/requestPasswordResetToken.php
+++ _protected/frontend/views/site/requestPasswordResetToken.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+    <p>A link to reset password will be sent to your email.</p>
 
     <div class="row">
         <div class="col-lg-12">
