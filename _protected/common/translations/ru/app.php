07a7d8a256ec99c11102ea0fba3fea5fde5e4d42
Ivan Bezdenezhnykh <ivan9876@gmail.com>
2015-04-20 23:16:49 +0300

    Russian translation

---
+++ _protected/common/translations/ru/app.php
@@ -0,0 +1,128 @@
+<?php
+
+return [
+
+    //-- general buttons --//
+    'Submit' => 'Отправить',
+    'Cancel' => 'Отменить',
+    'Update' => 'Обновить',
+    'Delete' => 'Удалить',
+    'Create' => 'Создать',
+    'Send'   => 'Отправить',
+    'Save'   => 'Сохранить',
+    'Menu'   => 'Меню',
+    'Back'   => 'Назад',
+
+    //-- CMS buttons --//
+    'Create User' => 'Создать пользователя',
+    'Create Article' => 'Создать статью',
+
+    //-- top menu --//
+    'Home'     => 'Главная',
+    'About'    => 'О сайте',
+    'Contact'  => 'Контакты',
+    'Login'    => 'Войти',
+    'Logout'   => 'Выйти',
+    'Users'    => 'Пользователи',
+    'Articles' => 'Статьи',
+    'Register' => 'Регистрация',
+
+    //-- static pages --//
+    'Password' => 'Пароль',
+    'Username' => 'Имя пользователя',
+    
+    // contact
+    'Name'    => 'Имя',
+    'Subject' => 'Тема',
+    'Text'    => 'Текст',
+    'Verification Code' => 'Код подтверждения',
+    'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.' 
+        => 'Если у вас есть предложения или вопросы, пожалуйста заполните форму ниже, чтобы связаться с нами. Спасибо.',
+    'Thank you for contacting us. We will respond to you as soon as possible.' 
+        => 'Спасибо за обращение. Мы ответим Вам, как только сможем.',
+    'There was an error sending email.' => 'Ошибка отправки email сообщения.',
+
+    // password reset
+    'If you forgot your password you can' => 'Если вы забыли Ваш пароль вы можете ',
+    'reset it' => 'сбросить его',
+    'Request password reset' => 'Запросить сброс пароля',
+    'Please fill out your email.' => 'Пожалуйста введите Ваш E-mail.',
+    'Reset password' => 'Сбросить пароль',
+    'A link to reset password will be sent to your email.' => 'Ссылка для сброса пароля будет отправлена на Ваш E-mail',
+    'Check your email for further instructions.' => 'Проверьте Ваш email для получения дальнейших инструкций.',
+    'Please choose your new password:' => 'Пожалуйста укажите новый пароль:',
+    'New password was saved.' => 'Новый пароль был сохранён.',
+    'Sorry, we are unable to reset password for email provided.' => 'К сожалению вы не можете сбросить пароль для данного email.',
+    'Password reset token cannot be blank.' => 'Код для сброса пароля не может быть пустым.',
+    'Wrong password reset token.' => 'Неверный код для сброса пароля.',
+
+    // signup
+    'Signup'   => 'Регистрация',
+    'Please fill out the following fields to signup:' => 'Введите поля ниже чтобы зарегистрироваться:',
+    'We will send you an email with account activation link.' => 'Вам будет выслано письмо со ссылкой для активации учетной записи.',
+    'We couldn\'t sign you up, please contact us.' => 'Не удалось создать учетную запись, пожалуйста свяжитесь с нами.',
+
+    // login
+    'Remember me'  => 'Запомнить меня',
+    'Please fill out the following fields to login:' => 'Введите поля ниже чтобы войти:',
+    'You have to activate your account first. Please check your email.' => 'Ваша учетная запись не активирована. Проверьте Ваш почтовый ящик.',
+    'To be able to log in, you need to confirm your registration. Please check your email, we have sent you a message.'
+        => 'Для того, чтобы войти, Вам необходимо подтвердить регистрацию. Пожалуйста проверьте Ваш почтовый ящик, мы выслали Вам сообщение.',
+
+    // account activation
+    'We couldn\'t send you account activation email, please contact us.' => 'Не удалось выслать письмо для активации учетной записи. Пожалуйста свяжитесь с нами.',
+    'Success! You can now log in.' => 'Успешно! Теперь Вы можете войти.',
+    'for joining us!' => 'что зарегистрировались!',
+    'your account could not be activated, please contact us!' => 'Ваша учетная запись не может быть активирована. Пожалуйста свяжитесь с нами.',
+    'Account activation token cannot be blank.' => 'Код активации не должен быть пустым.',
+    'Wrong account activation token.' => 'Неверный код активации.',
+
+    //-- general db fields --//
+    'Created At' => 'Создано',
+    'Updated At' => 'Обновлено',
+
+    //-- mixed --//
+    'My Company' => 'Моя компания',
+    'Hello' => 'Здравствуйте',
+    'Thank you' => 'Спасибо',
+    
+    //-- users management --//
+    'Role' => 'Роль',
+    'Create User' => 'Создать пользователя',
+    'Update User' => 'Отредактировать пользователя',
+    'New pwd ( if you want to change it )' => 'Новый пароль ( если хотите изменить )',
+
+    //-- articles --//
+    'Title'    => 'Заголовок',
+    'Summary'  => 'Краткое описание',
+    'Content'  => 'Содержимое',
+    'Status'   => 'Статус',
+    'Category' => 'Категория',
+    'Author'   => 'Автор',
+    'articles' => 'статьи',
+    'news'     => 'новости',
+    'The best news available' => 'Лучшие новости',
+    'We haven\'t created any articles yet.' => 'Пока нет ни одной статьи.',
+    'Read more'    => 'Читать дальше',
+    'Published on' => 'Опубликовано',
+
+    // statuses
+    'Draft'     => 'Черновик',
+    'Published' => 'Опубликовано',
+    'Archived'  => 'В архиве',
+
+    // categories
+    'Economy' => 'Экономика',
+    'Society' => 'Общество',
+    'Sport'   => 'Спорт',
+
+    //-- errors --//
+    'The above error occurred while the Web server was processing your request.' => 'Ошибка возникла при обработке запроса Web сервером.',
+    'Please contact us if you think this is a server error. Thank you.' => 'Пожалуйста свяжитесь с нами, если вы считаете, что это ошибка сервера. Спасибо.',
+    'You are not allowed to access this page.' => 'Недостаточно прав для просмотра данной страницы.',
+
+    //-- delete messages --//
+    'Are you sure you want to delete this user?' => 'Вы уверены, что хотите удалить данного пользователя?',
+    'Are you sure you want to delete this article?' => 'Вы уверены, что хотите удалить данную статью?',
+
+];
