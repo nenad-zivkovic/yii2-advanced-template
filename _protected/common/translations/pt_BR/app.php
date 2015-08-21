4165eec54f42dbe8d440ed13aa83b4eb0a37d0c2
rodzadra <rodzadra@gmail.com>
2015-08-21 16:00:38 -0300

    Brazilian portuguese translation (pt_BR)

---
+++ _protected/common/translations/pt_BR/app.php
@@ -0,0 +1,127 @@
+<?php
+
+return [
+
+    //-- general buttons --//
+    'Submit' => 'Enviar',
+    'Cancel' => 'Cancelar',
+    'Update' => 'Atualizar',
+    'Delete' => 'Apagar',
+    'Create' => 'Criar',
+    'Send'   => 'Enviar',
+    'Save'   => 'Gravar',
+    'Menu'   => 'Menu',
+    'Back'   => 'Voltar',
+
+    //-- CMS buttons --//
+    'Create User' => 'Novo usuário',
+    'Create Article' => 'Novo artigo',
+
+    //-- top menu --//
+    'Home'     => 'Início',
+    'About'    => 'Quem somos',
+    'Contact'  => 'Contato',
+    'Login'    => 'Login',
+    'Logout'   => 'Logout',
+    'Users'    => 'Usuários',
+    'Articles' => 'Notícias',
+    'Register' => 'Cadastre-se',
+
+    //-- static pages --//
+    'Password' => 'Senha',
+    'Username' => 'Nome de usuário',
+    
+    // contact
+    'Name'    => 'Nome',
+    'Subject' => 'Assunto',
+    'Text'    => 'Mensagem',
+    'Verification Code' => 'Código de verificação',
+    'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.' 
+        => 'Por favor, utilize o formulário abaixo para entrar em contato conosco. Obrigado.',
+    'Thank you for contacting us. We will respond to you as soon as possible.' => 'Obrigado. Entraremos em contato o mais breve possível.',
+    'There was an error sending email.' => 'Ocorreu um erro ao enviar sua mensagem!',
+
+    // password reset
+    'If you forgot your password you can' => 'Se você se esqueceu da senha, você pode ',
+    'reset it' => 'recuperá-la',
+    'Request password reset' => 'Recuperar senha',
+    'Please fill out your email.' => 'Por favor, digite seu email.',
+    'Reset password' => 'Criar uma nova senha.',
+    'A link to reset password will be sent to your email.' => 'Um link para recuperação da senha será enviado ao seu email.',
+    'Check your email for further instructions.' => 'Verifique seu email e siga as instruções.',
+    'Please choose your new password:' => 'Por favor, digite uma nova senha:',
+    'New password was saved.' => 'Nova senha salva.',
+    'Sorry, we are unable to reset password for email provided.' => 'Infelizmente não foi possível resetar a senha para o email fornecido.',
+    'Password reset token cannot be blank.' => 'O token para resetar a senha não pode estar em branco.',
+    'Wrong password reset token.' => 'Token para resetar a senha está errado ou é inválido.',
+
+    // signup
+    'Signup'   => 'Cadastre-se',
+    'Please fill out the following fields to signup:' => 'Por favor, preencha os seguintes campos para se cadastrar:',
+    'We will send you an email with account activation link.' => 'Nós mandaremos um email com o link para ativação de sua conta.',
+    'We couldn\'t sign you up, please contact us.' => 'Não foi possível te registrar! Por favor, entre em contato conosco.',
+
+    // login
+    'Remember me'  => 'Lembrar',
+    'Please fill out the following fields to login:' => 'Por favor, preencha os campos para efetuar o login:',
+    'You have to activate your account first. Please check your email.' => 'Você precisa ativar sua conta primeiro. Por favor, verifique seu email.',
+    'To be able to log in, you need to confirm your registration. Please check your email, we have sent you a message.'
+        => 'Para você poder se logar, é necessário confirmar seu registro. Por favor, verifique seu email, nós te enviamos uma mensagem.',
+
+    // account activation
+    'We couldn\'t send you account activation email, please contact us.' => 'Nós não conseguimos te enviar uma mensagem de confirmação! Por favor, entre em contato conosco.',
+    'Success! You can now log in.' => 'Sucesso! Agora você pode fazer seu login.',
+    'for joining us!' => 'Para unir-se à nós!',
+    'your account could not be activated, please contact us!' => 'Sua conta não pode ser ativada! Por favor, entre em contato conosco.',
+    'Account activation token cannot be blank.' => 'Conta inválida! O token não pode estar em branco.',
+    'Wrong account activation token.' => 'Token da conta errado ou inválido.',
+
+    //-- general db fields --//
+    'Created At' => 'Criado em',
+    'Updated At' => 'Modificado em',
+
+    //-- mixed --//
+    'My Company' => 'Minha compania',
+    'Hello' => 'Olá',
+    'Thank you' => 'Obrigado',
+    
+    //-- users management --//
+    'Role' => 'Role',
+    'Create User' => 'Criar usuário',
+    'Update User' => 'Modificar usuário',
+    'New pwd ( if you want to change it )' => 'Nova senha (se você quiser mudá-la)',
+
+    //-- articles --//
+    'Title'    => 'Título',
+    'Summary'  => 'Sumário',
+    'Content'  => 'Conteúdo',
+    'Status'   => 'Status',
+    'Category' => 'Categoria',
+    'Author'   => 'Autor',
+    'articles' => 'Artigos',
+    'news'     => 'Notícias',
+    'The best news available' => 'Últimas notícias',
+    'We haven\'t created any articles yet.' => 'Nós ainda não temos nenhum artigo publicado.',
+    'Read more'    => 'Ler mais',
+    'Published on' => 'Publicado em',
+	
+    // statuses
+    'Draft'     => 'Rascunho',
+    'Published' => 'Publicado',
+    'Archived'  => 'Arquivado',
+
+    // categories
+    'Economy' => 'Economia',
+    'Society' => 'Sociedade',
+    'Sport'   => 'Esportes',
+
+    //-- errors --//
+    'The above error occurred while the Web server was processing your request.' => 'O erro acima ocorreu quando o servidor tentava processar seu pedido.',
+    'Please contact us if you think this is a server error. Thank you.' => 'Se você achar que este erro não deveria estar ocorrendo, por favor, entre em contato conosco. Obrigado.',
+    'You are not allowed to access this page.' => 'Você não tem permissão para acessar esta página.',
+
+    //-- delete messages --//
+    'Are you sure you want to delete this user?' => 'Tem certeza que quer apagar este usuário?',
+    'Are you sure you want to delete this article?' => 'Tem certeza que quer apagar este artigo?',
+
+];
---
+++ _protected/common/translations/pt_BR/yii.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '{attribute} cannot be blank.' => '{attribute} não pode estar em branco.',
+    'The verification code is incorrect.' => 'Código de verificação incorreto.',
+    'Home' => 'Início',
+    'You are not allowed to perform this action.' => 'Você não está autorizado a realizar esta ação.'
+];
