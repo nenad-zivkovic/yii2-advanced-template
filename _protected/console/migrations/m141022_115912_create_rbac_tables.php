c26dac4fdd6bd4772a1095157f42e346356053c0
Nenad Zivkovic <nenad@freetuts.org>
2015-09-20 20:15:06 +0200

    Fixes rbac migration

--- _protected/console/migrations/m141022_115912_create_rbac_tables.php
+++ _protected/console/migrations/m141022_115912_create_rbac_tables.php
@@ -12,7 +12,7 @@ use yii\rbac\DbManager;
  * @author Alexander Kochetov <creocoder@gmail.com>
  * @since 2.0
  */
-class m140506_102106_rbac_init extends \yii\db\Migration
+class m1401022_115912_rbac_init extends \yii\db\Migration
 {
     /**
      * @throws yii\base\InvalidConfigException
