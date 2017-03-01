aa6e24ce2a96552dd9b0356441deaaeda378ad5c
Jerry <soso_1983@msn.com>
2017-03-01 12:46:34 +0800

    Simplified Chinese translations

---
+++ _protected/common/translations/zh-CN/yii.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '{attribute} cannot be blank.' => '{attribute}不能为空。',
+    'The verification code is incorrect.' => '验证码错误。',
+    'Home' => '首页',
+    'You are not allowed to perform this action.' => '您不能执行此操作。'
+];
